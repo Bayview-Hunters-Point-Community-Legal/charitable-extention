2733aaef42884942c9be981fd866b43f84448070
Spencer Barton <sbarton272@gmail.com>
2017-03-05 14:35:05 -0800

    Move to css folder

--- templates/campaign/featured-image.php
+++ templates/campaign/featured-image.php
@@ -10,20 +10,9 @@
 
 ?>
 
-<div class="charitable-campaign-featured-image" style="
-  background: url('<?php the_post_thumbnail_url( 'full' ); ?>');
-  position: relative;
-  height: 500px;
-  background-size: cover;
-  background-repeat: no-repeat;">
+<div class="charitable-campaign-featured-image" style="background: url('<?php the_post_thumbnail_url( 'full' ); ?>');">
 </div>
 
-<div class="container case-title-container" style="padding-bottom: -10px;">
-  <h1 class="case-title" style="
-    position: absolute;
-    top: -60px;
-    color: white;
-    text-shadow: 1px 1px #000000;">
-    <?php the_title() ?>
-  </h1>
+<div id="charitable-campaign-title-container" class="container charitable-campaign-title-container">
+  <?php the_title() ?>
 </div>
